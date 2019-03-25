81f7f58c82fed96dfacacf143b3553def945e06f
Joshua <jiangxn71@163.com>
2019-03-25 17:30:30 +0800

    :fire: feat: rxBus observer

---
+++ bscommonlib/src/main/java/com/bestseller/commonlib/base/rx/RxBusObservable.java
@@ -0,0 +1,21 @@
+package com.bestseller.commonlib.base.rx;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * Author:Joshua
+ * Date:2019/3/25
+ * Description:
+ */
+public abstract class RxBusObservable implements Observer {
+    @Override
+    public void onSubscribe(Disposable d) {
+
+    }
+
+    @Override
+    public void onComplete() {
+
+    }
+}
