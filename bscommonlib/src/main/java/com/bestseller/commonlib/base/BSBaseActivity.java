4652a46beac67decb6241a35f05affb327cd63c0
Joshua <jiangxn71@163.com>
2019-03-26 18:31:19 +0800

    :fire: fix: ui fix

--- bscommonlib/src/main/java/com/bestseller/commonlib/base/BSBaseActivity.java
+++ bscommonlib/src/main/java/com/bestseller/commonlib/base/BSBaseActivity.java
@@ -6,14 +6,10 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
 import com.orhanobut.logger.Logger;
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
@@ -31,8 +27,6 @@ public abstract class BSBaseActivity extends RxAppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        hideStatusBar();
-
         context = this;
         mLoadingDialog = new BaseLoadingDialog(this);
         builder = new AlertDialog.Builder(this).setTitle("提示");
@@ -45,16 +39,6 @@ public abstract class BSBaseActivity extends RxAppCompatActivity {
 
     }
 
-    private void hideStatusBar() {
-        Window window = activity.getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(Color.BLACK);
-        window.setNavigationBarColor(Color.BLACK);
-    }
 
     protected abstract void initView(Bundle savedInstanceState);
 
