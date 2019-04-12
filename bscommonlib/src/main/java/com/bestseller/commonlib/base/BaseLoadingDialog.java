e8d7702db00457820d7414bbfe1e87e9aa8d30b9
Joshua <jiangxn71@163.com>
2019-04-12 17:42:11 +0800

    :fire: style: loading dialog ui update

--- bscommonlib/src/main/java/com/bestseller/commonlib/base/BaseLoadingDialog.java
+++ bscommonlib/src/main/java/com/bestseller/commonlib/base/BaseLoadingDialog.java
@@ -2,11 +2,8 @@ package com.bestseller.commonlib.base;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
-import android.util.DisplayMetrics;
-import android.view.Gravity;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ProgressBar;
@@ -26,24 +23,8 @@ public class BaseLoadingDialog extends Dialog {
     private String defaultTip = "加载中...";
 
 
-    public void setTip(String tip) {
-        if (progressMsg000 != null) {
-            progressMsg000.setText(tip);
-        }
-    }
-
-    public void setDefaultTip() {
-        if (progressMsg000 != null) {
-            progressMsg000.setText(defaultTip);
-        }
-    }
-
     public BaseLoadingDialog(Context context) {
         super(context, R.style.Theme_bs_base_loading_dialog);
-        setContentView(R.layout.bs_base_dialog_loading);
-        progressMsg000 = findViewById(R.id.progressMsg000);
-        loadingBar = findViewById(R.id.bsBaseProgressBar000);
-        setCancelable(true);
         Window window = getWindow();
         if (window != null) {
             WindowManager.LayoutParams params = window.getAttributes();
@@ -53,23 +34,24 @@ public class BaseLoadingDialog extends Dialog {
 //            params.height = (int) (height * density);
             params.width = WindowManager.LayoutParams.MATCH_PARENT;
             params.height = WindowManager.LayoutParams.MATCH_PARENT;
-            params.gravity = Gravity.CENTER_HORIZONTAL;
             window.setBackgroundDrawable(new ColorDrawable(context.getResources().getColor(R.color.color_base_loading_dialog_bg)));
             window.setAttributes(params);
-            loadingBar.getIndeterminateDrawable().setColorFilter(context.getResources().getColor
-                    (R.color.colorPrimary), PorterDuff.Mode.SRC_ATOP);
+        }
+        setContentView(R.layout.bs_base_dialog_loading);
+        progressMsg000 = findViewById(R.id.progressMsg000);
+        loadingBar = findViewById(R.id.bsBaseProgressBar000);
+        loadingBar.getIndeterminateDrawable().setColorFilter(context.getResources().getColor
+                (R.color.colorPrimary), PorterDuff.Mode.SRC_ATOP);
+        setCancelable(true);
+    }
+    public void setTip(String tip) {
+        if (progressMsg000 != null) {
+            progressMsg000.setText(tip);
         }
     }
-
-    /**
-     * 获取显示密度
-     *
-     * @param context
-     * @return
-     */
-    public float getDensity(Context context) {
-        Resources res = context.getResources();
-        DisplayMetrics dm = res.getDisplayMetrics();
-        return dm.density;
+    public void setDefaultTip() {
+        if (progressMsg000 != null) {
+            progressMsg000.setText(defaultTip);
+        }
     }
 }
