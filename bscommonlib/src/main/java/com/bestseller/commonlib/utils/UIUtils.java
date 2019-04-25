055d1e611d1605e54f512d4d968141bd8aa32cd6
Joshua <jiangxn71@163.com>
2019-04-26 01:19:15 +0800

    :fire: feat: add UI util

---
+++ bscommonlib/src/main/java/com/bestseller/commonlib/utils/UIUtils.java
@@ -0,0 +1,84 @@
+package com.bestseller.shoppingguide.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.support.design.widget.TabLayout;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.orhanobut.logger.Logger;
+
+import java.lang.reflect.Field;
+
+
+/**
+ * Created by joshua on 2017/4/17.
+ */
+
+public class UIUtils {
+
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    public static void setMargins(View v, int l, int t, int r, int b) {
+        if (v.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            p.setMargins(l, t, r, b);
+            v.requestLayout();
+        }
+    }
+
+    /**
+     * 设置水印图片到中间
+     * @param src
+     * @param watermark
+     * @return
+     */
+    public static Bitmap createWaterMaskCenter(Bitmap src, Bitmap watermark) {
+        return createWaterMaskBitmap(src, watermark,
+                (src.getWidth() - watermark.getWidth()) / 2,
+                (src.getHeight() - watermark.getHeight()) / 2);
+    }
+
+    private static Bitmap createWaterMaskBitmap(Bitmap src, Bitmap watermark,
+                                                int paddingLeft, int paddingTop) {
+        if (src == null) {
+            return null;
+        }
+        int width = src.getWidth();
+        int height = src.getHeight();
+        //创建一个bitmap
+        Bitmap newb = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);// 创建一个新的和SRC长度宽度一样的位图
+        //将该图片作为画布
+        Canvas canvas = new Canvas(newb);
+        //在画布 0，0坐标上开始绘制原始图片
+        canvas.drawBitmap(src, 0, 0, null);
+        //在画布上绘制水印图片
+        canvas.drawBitmap(watermark, paddingLeft, paddingTop, null);
+        // 保存
+        canvas.save();
+        // 存储
+        canvas.restore();
+        return newb;
+    }
+
+
+}
