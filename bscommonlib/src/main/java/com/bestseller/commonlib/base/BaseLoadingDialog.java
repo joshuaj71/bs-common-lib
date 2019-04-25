b2165bcaaaf36ffa31b004bf1b788119e225be2b
Joshua <jiangxn71@163.com>
2019-04-25 23:04:10 +0800

    :fire: style: loading dialog ui update

--- bscommonlib/src/main/java/com/bestseller/commonlib/base/BaseLoadingDialog.java
+++ bscommonlib/src/main/java/com/bestseller/commonlib/base/BaseLoadingDialog.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
 import android.view.Gravity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ProgressBar;
@@ -55,5 +56,24 @@ public class BaseLoadingDialog extends Dialog {
         if (progressMsg000 != null) {
             progressMsg000.setText(defaultTip);
         }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSysBars();
+        }
+    }
+
+    private void hideSysBars() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
